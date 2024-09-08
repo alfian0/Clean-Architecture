344c9af41922422da9034dc5e7222479339c4112
alfian0 <alfian.official.mail@gmail.com>
2024-09-08 19:22:56 +0700

    Add Hashable and Coordinator

---
+++ ___FILEBASENAME___/___FILEBASENAME___Coordinator.swift
@@ -0,0 +1,27 @@
+// Created with MVVM 2024
+// Git Repo: https://github.com/alfian0/Clean-Architecture.git
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+import SwiftUI
+
+final class ___VARIABLE_productName___Coordinator: Coordinator {
+	var childCoordinator: [Coordinator] = [Coordinator]()
+	var navigationController: UINavigationController
+	
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+	
+	func start(with params: ___VARIABLE_productName___PageViewParamsImpl) {
+		let view = AppContainerImpl.shared.get___VARIABLE_productName___Page(params: params)
+		let viewController = UIHostingController(rootView: view)
+		viewController.hidesBottomBarWhenPushed = true
+		
+		navigationController.pushViewController(viewController, animated: true)
+	}
+}
