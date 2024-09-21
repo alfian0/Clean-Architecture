d9b51a6f7c4a333ab34b2486d127c6a603c521e2
alfian0 <alfian.official.mail@gmail.com>
2024-09-21 19:11:21 +0700

    Add adapter

---
+++ Data.xctemplate/___FILEBASENAME___DTOAdapter.swift
@@ -0,0 +1,19 @@
+// Created with MVVM 2024
+// Git Repo: https://github.com/alfian0/Clean-Architecture.git
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import Foundation
+
+class ___VARIABLE_productName___DTOAdapter {
+	static func toDTO(response: ___VARIABLE_productName___Entity) -> ___VARIABLE_productName___DTO {
+		return ___VARIABLE_productName___DTO()
+	}
+	
+	static func toDTO(response: ___VARIABLE_productName___Response) -> ___VARIABLE_productName___DTO {
+		return ___VARIABLE_productName___DTO()
+	}
+}
